96e60ff7993a0086bc9877934e48a3c114371941
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-03-29 15:17:47 +0700

    bookmarks: sources

---
+++ modules/inlis/views/o/bookmark/_search.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Inlis Bookmarks (inlis-bookmarks)
+ * @var $this BookmarkController
+ * @var $model InlisBookmarks
+ * @var $form CActiveForm
+ * version: 0.0.1
+ *
+ * @author Putra Sudaryanto <putra.sudaryanto@gmail.com>
+ * @copyright Copyright (c) 2016 Ommu Platform (ommu.co)
+ * @created date 29 March 2016, 15:14 WIB
+ * @link http://company.ommu.co
+ * @contect (+62)856-299-4114
+ *
+ */
+?>
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'action'=>Yii::app()->createUrl($this->route),
+	'method'=>'get',
+)); ?>
+	<ul>
+		<li>
+			<?php echo $model->getAttributeLabel('bookmark_id'); ?><br/>
+			<?php echo $form->textField($model,'bookmark_id',array('size'=>11,'maxlength'=>11)); ?>
+		</li>
+
+		<li>
+			<?php echo $model->getAttributeLabel('catalog_id'); ?><br/>
+			<?php echo $form->textField($model,'catalog_id',array('size'=>11,'maxlength'=>11)); ?>
+		</li>
+
+		<li>
+			<?php echo $model->getAttributeLabel('user_id'); ?><br/>
+			<?php echo $form->textField($model,'user_id',array('size'=>11,'maxlength'=>11)); ?>
+		</li>
+
+		<li>
+			<?php echo $model->getAttributeLabel('creation_date'); ?><br/>
+			<?php echo $form->textField($model,'creation_date'); ?>
+		</li>
+
+		<li>
+			<?php echo $model->getAttributeLabel('creation_ip'); ?><br/>
+			<?php echo $form->textField($model,'creation_ip',array('size'=>20,'maxlength'=>20)); ?>
+		</li>
+
+		<li class="submit">
+			<?php echo CHtml::submitButton(Yii::t('phrase', 'Search')); ?>
+		</li>
+	</ul>
+<?php $this->endWidget(); ?>
