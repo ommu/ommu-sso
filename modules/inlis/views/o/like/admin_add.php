ed88d68dd71b88ed5066409b9d25a36831ec886f
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-03-29 15:18:43 +0700

    likes: sources

---
+++ modules/inlis/views/o/like/admin_add.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Inlis Likes (inlis-likes)
+ * @var $this LikeController
+ * @var $model InlisLikes
+ * @var $form CActiveForm
+ * version: 0.0.1
+ *
+ * @author Putra Sudaryanto <putra.sudaryanto@gmail.com>
+ * @copyright Copyright (c) 2016 Ommu Platform (ommu.co)
+ * @created date 29 March 2016, 15:15 WIB
+ * @link http://company.ommu.co
+ * @contect (+62)856-299-4114
+ *
+ */
+
+	$this->breadcrumbs=array(
+		'Inlis Likes'=>array('manage'),
+		'Create',
+	);
+?>
+
+<div class="form">
+	<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+</div>
