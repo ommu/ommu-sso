1cfb45fe0ebaa6abc3f6f965cba4430a9259d2a7
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-03-29 15:18:58 +0700

    catalogs: sources

---
+++ modules/inlis/models/InlisCatalogs.php
@@ -0,0 +1,300 @@
+<?php
+/**
+ * InlisCatalogs
+ * version: 0.0.1
+ *
+ * @author Putra Sudaryanto <putra.sudaryanto@gmail.com>
+ * @copyright Copyright (c) 2016 Ommu Platform (ommu.co)
+ * @created date 29 March 2016, 15:13 WIB
+ * @link http://company.ommu.co
+ * @contact (+62)856-299-4114
+ *
+ * This is the template for generating the model class of a specified table.
+ * - $this: the ModelCode object
+ * - $tableName: the table name for this class (prefix is already removed if necessary)
+ * - $modelClass: the model class name
+ * - $columns: list of table columns (name=>CDbColumnSchema)
+ * - $labels: list of attribute labels (name=>label)
+ * - $rules: list of validation rules
+ * - $relations: list of relations (name=>relation declaration)
+ *
+ * --------------------------------------------------------------------------------------
+ *
+ * This is the model class for table "ommu_inlis_catalogs".
+ *
+ * The followings are the available columns in table 'ommu_inlis_catalogs':
+ * @property string $id
+ * @property string $catalog_id
+ * @property string $creation_date
+ * @property string $creation_id
+ */
+class InlisCatalogs extends CActiveRecord
+{
+	public $defaultColumns = array();
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return InlisCatalogs the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'ommu_inlis_catalogs';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('catalog_id, creation_date, creation_id', 'required'),
+			array('catalog_id', 'length', 'max'=>11),
+			array('creation_id', 'length', 'max'=>10),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('id, catalog_id, creation_date, creation_id', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => Yii::t('attribute', 'ID'),
+			'catalog_id' => Yii::t('attribute', 'Catalog'),
+			'creation_date' => Yii::t('attribute', 'Creation Date'),
+			'creation_id' => Yii::t('attribute', 'Creation'),
+		);
+		/*
+			'ID' => 'ID',
+			'Catalog' => 'Catalog',
+			'Creation Date' => 'Creation Date',
+			'Creation' => 'Creation',
+		
+		*/
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('t.id',strtolower($this->id),true);
+		$criteria->compare('t.catalog_id',strtolower($this->catalog_id),true);
+		if($this->creation_date != null && !in_array($this->creation_date, array('0000-00-00 00:00:00', '0000-00-00')))
+			$criteria->compare('date(t.creation_date)',date('Y-m-d', strtotime($this->creation_date)));
+		if(isset($_GET['creation']))
+			$criteria->compare('t.creation_id',$_GET['creation']);
+		else
+			$criteria->compare('t.creation_id',$this->creation_id);
+
+		if(!isset($_GET['InlisCatalogs_sort']))
+			$criteria->order = 't.id DESC';
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+			'pagination'=>array(
+				'pageSize'=>30,
+			),
+		));
+	}
+
+
+	/**
+	 * Get column for CGrid View
+	 */
+	public function getGridColumn($columns=null) {
+		if($columns !== null) {
+			foreach($columns as $val) {
+				/*
+				if(trim($val) == 'enabled') {
+					$this->defaultColumns[] = array(
+						'name'  => 'enabled',
+						'value' => '$data->enabled == 1? "Ya": "Tidak"',
+					);
+				}
+				*/
+				$this->defaultColumns[] = $val;
+			}
+		} else {
+			//$this->defaultColumns[] = 'id';
+			$this->defaultColumns[] = 'catalog_id';
+			$this->defaultColumns[] = 'creation_date';
+			$this->defaultColumns[] = 'creation_id';
+		}
+
+		return $this->defaultColumns;
+	}
+
+	/**
+	 * Set default columns to display
+	 */
+	protected function afterConstruct() {
+		if(count($this->defaultColumns) == 0) {
+			/*
+			$this->defaultColumns[] = array(
+				'class' => 'CCheckBoxColumn',
+				'name' => 'id',
+				'selectableRows' => 2,
+				'checkBoxHtmlOptions' => array('name' => 'trash_id[]')
+			);
+			*/
+			$this->defaultColumns[] = array(
+				'header' => 'No',
+				'value' => '$this->grid->dataProvider->pagination->currentPage*$this->grid->dataProvider->pagination->pageSize + $row+1'
+			);
+			$this->defaultColumns[] = 'catalog_id';
+			$this->defaultColumns[] = array(
+				'name' => 'creation_date',
+				'value' => 'Utility::dateFormat($data->creation_date)',
+				'htmlOptions' => array(
+					'class' => 'center',
+				),
+				'filter' => Yii::app()->controller->widget('zii.widgets.jui.CJuiDatePicker', array(
+					'model'=>$this,
+					'attribute'=>'creation_date',
+					'language' => 'ja',
+					'i18nScriptFile' => 'jquery.ui.datepicker-en.js',
+					//'mode'=>'datetime',
+					'htmlOptions' => array(
+						'id' => 'creation_date_filter',
+					),
+					'options'=>array(
+						'showOn' => 'focus',
+						'dateFormat' => 'dd-mm-yy',
+						'showOtherMonths' => true,
+						'selectOtherMonths' => true,
+						'changeMonth' => true,
+						'changeYear' => true,
+						'showButtonPanel' => true,
+					),
+				), true),
+			);
+			$this->defaultColumns[] = 'creation_id';
+		}
+		parent::afterConstruct();
+	}
+
+	/**
+	 * User get information
+	 */
+	public static function getInfo($id, $column=null)
+	{
+		if($column != null) {
+			$model = self::model()->findByPk($id,array(
+				'select' => $column
+			));
+			return $model->$column;
+			
+		} else {
+			$model = self::model()->findByPk($id);
+			return $model;			
+		}
+	}
+
+	/**
+	 * before validate attributes
+	 */
+	/*
+	protected function beforeValidate() {
+		if(parent::beforeValidate()) {
+			// Create action
+		}
+		return true;
+	}
+	*/
+
+	/**
+	 * after validate attributes
+	 */
+	/*
+	protected function afterValidate()
+	{
+		parent::afterValidate();
+			// Create action
+		return true;
+	}
+	*/
+	
+	/**
+	 * before save attributes
+	 */
+	/*
+	protected function beforeSave() {
+		if(parent::beforeSave()) {
+		}
+		return true;	
+	}
+	*/
+	
+	/**
+	 * After save attributes
+	 */
+	/*
+	protected function afterSave() {
+		parent::afterSave();
+		// Create action
+	}
+	*/
+
+	/**
+	 * Before delete attributes
+	 */
+	/*
+	protected function beforeDelete() {
+		if(parent::beforeDelete()) {
+			// Create action
+		}
+		return true;
+	}
+	*/
+
+	/**
+	 * After delete attributes
+	 */
+	/*
+	protected function afterDelete() {
+		parent::afterDelete();
+		// Create action
+	}
+	*/
+
+}
---
+++ modules/inlis/views/o/admin/_search.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Inlis Catalogs (inlis-catalogs)
+ * @var $this AdminController
+ * @var $model InlisCatalogs
+ * @var $form CActiveForm
+ * version: 0.0.1
+ *
+ * @author Putra Sudaryanto <putra.sudaryanto@gmail.com>
+ * @copyright Copyright (c) 2016 Ommu Platform (ommu.co)
+ * @created date 29 March 2016, 15:15 WIB
+ * @link http://company.ommu.co
+ * @contect (+62)856-299-4114
+ *
+ */
+?>
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'action'=>Yii::app()->createUrl($this->route),
+	'method'=>'get',
+)); ?>
+	<ul>
+		<li>
+			<?php echo $model->getAttributeLabel('id'); ?><br/>
+			<?php echo $form->textField($model,'id',array('size'=>11,'maxlength'=>11)); ?>
+		</li>
+
+		<li>
+			<?php echo $model->getAttributeLabel('catalog_id'); ?><br/>
+			<?php echo $form->textField($model,'catalog_id',array('size'=>11,'maxlength'=>11)); ?>
+		</li>
+
+		<li>
+			<?php echo $model->getAttributeLabel('creation_date'); ?><br/>
+			<?php echo $form->textField($model,'creation_date'); ?>
+		</li>
+
+		<li>
+			<?php echo $model->getAttributeLabel('creation_id'); ?><br/>
+			<?php echo $form->textField($model,'creation_id',array('size'=>10,'maxlength'=>10)); ?>
+		</li>
+
+		<li class="submit">
+			<?php echo CHtml::submitButton(Yii::t('phrase', 'Search')); ?>
+		</li>
+	</ul>
+<?php $this->endWidget(); ?>
