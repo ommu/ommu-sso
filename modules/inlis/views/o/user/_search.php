d73eb48ceb221448edf405b96d0645aef6511c04
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-03-29 16:15:49 +0700

    users: add source

---
+++ modules/inlis/views/o/user/_search.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Inlis Users (inlis-users)
+ * @var $this UserController
+ * @var $model InlisUsers
+ * @var $form CActiveForm
+ * version: 0.0.1
+ *
+ * @author Putra Sudaryanto <putra.sudaryanto@gmail.com>
+ * @copyright Copyright (c) 2016 Ommu Platform (ommu.co)
+ * @created date 29 March 2016, 16:14 WIB
+ * @link http://company.ommu.co
+ * @contect (+62)856-299-4114
+ *
+ */
+?>
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'action'=>Yii::app()->createUrl($this->route),
+	'method'=>'get',
+)); ?>
+	<ul>
+		<li>
+			<?php echo $model->getAttributeLabel('id'); ?><br/>
+			<?php echo $form->textField($model,'id',array('size'=>11,'maxlength'=>11)); ?>
+		</li>
+
+		<li>
+			<?php echo $model->getAttributeLabel('user_id'); ?><br/>
+			<?php echo $form->textField($model,'user_id',array('size'=>11,'maxlength'=>11)); ?>
+		</li>
+
+		<li>
+			<?php echo $model->getAttributeLabel('member_id'); ?><br/>
+			<?php echo $form->textField($model,'member_id',array('size'=>11,'maxlength'=>11)); ?>
+		</li>
+
+		<li>
+			<?php echo $model->getAttributeLabel('creation_date'); ?><br/>
+			<?php echo $form->textField($model,'creation_date'); ?>
+		</li>
+
+		<li>
+			<?php echo $model->getAttributeLabel('creation_id'); ?><br/>
+			<?php echo $form->textField($model,'creation_id',array('size'=>11,'maxlength'=>11)); ?>
+		</li>
+
+		<li>
+			<?php echo $model->getAttributeLabel('modified_date'); ?><br/>
+			<?php echo $form->textField($model,'modified_date'); ?>
+		</li>
+
+		<li>
+			<?php echo $model->getAttributeLabel('modified_id'); ?><br/>
+			<?php echo $form->textField($model,'modified_id',array('size'=>11,'maxlength'=>11)); ?>
+		</li>
+
+		<li class="submit">
+			<?php echo CHtml::submitButton(Yii::t('phrase', 'Search')); ?>
+		</li>
+	</ul>
+<?php $this->endWidget(); ?>
---
+++ modules/inlis/views/o/user/admin_edit.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Inlis Users (inlis-users)
+ * @var $this UserController
+ * @var $model InlisUsers
+ * @var $form CActiveForm
+ * version: 0.0.1
+ *
+ * @author Putra Sudaryanto <putra.sudaryanto@gmail.com>
+ * @copyright Copyright (c) 2016 Ommu Platform (ommu.co)
+ * @created date 29 March 2016, 16:14 WIB
+ * @link http://company.ommu.co
+ * @contect (+62)856-299-4114
+ *
+ */
+
+	$this->breadcrumbs=array(
+		'Inlis Users'=>array('manage'),
+		$model->id=>array('view','id'=>$model->id),
+		'Update',
+	);
+?>
+
+<div class="form">
+	<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+</div>
