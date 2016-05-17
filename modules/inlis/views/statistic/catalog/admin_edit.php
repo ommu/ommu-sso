eaa7a80b19df57f05c039650be53262a4a32f9b3
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-05-17 17:27:08 +0700

    statistic: catalog source

---
+++ modules/inlis/views/statistic/catalog/admin_edit.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * View Inlis Sync Catalogs (view-inlis-sync-catalogs)
+ * @var $this CatalogController
+ * @var $model ViewInlisSyncCatalogs
+ * @var $form CActiveForm
+ * version: 0.0.1
+ *
+ * @author Putra Sudaryanto <putra.sudaryanto@gmail.com>
+ * @copyright Copyright (c) 2016 Ommu Platform (ommu.co)
+ * @created date 17 May 2016, 17:24 WIB
+ * @link http://company.ommu.co
+ * @contect (+62)856-299-4114
+ *
+ */
+
+	$this->breadcrumbs=array(
+		'View Inlis Sync Catalogs'=>array('manage'),
+		$model->date_key=>array('view','id'=>$model->date_key),
+		'Update',
+	);
+?>
+
+<div class="form">
+	<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+</div>
---
+++ modules/inlis/views/statistic/catalog/admin_publish.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * View Inlis Sync Catalogs (view-inlis-sync-catalogs)
+ * @var $this CatalogController
+ * @var $model ViewInlisSyncCatalogs
+ * @var $form CActiveForm
+ * version: 0.0.1
+ *
+ * @author Putra Sudaryanto <putra.sudaryanto@gmail.com>
+ * @copyright Copyright (c) 2016 Ommu Platform (ommu.co)
+ * @created date 17 May 2016, 17:24 WIB
+ * @link http://company.ommu.co
+ * @contect (+62)856-299-4114
+ *
+ */
+
+	$this->breadcrumbs=array(
+		'View Inlis Sync Catalogs'=>array('manage'),
+		'Publish',
+	);
+?>
+
+<?php $form=$this->beginWidget('application.components.system.OActiveForm', array(
+	'id'=>'view-inlis-sync-catalogs-form',
+	'enableAjaxValidation'=>true,
+	//'htmlOptions' => array('enctype' => 'multipart/form-data')
+)); ?>
+
+	<div class="dialog-content">
+		<?php echo $model->publish == 1 ? Yii::t('phrase', 'Are you sure you want to unpublish this item?') : Yii::t('phrase', 'Are you sure you want to publish this item?')?>
+		<?php //echo $model->actived == 1 ? Yii::t('phrase', 'Are you sure you want to deactived this item?') : Yii::t('phrase', 'Are you sure you want to actived this item?')?>
+		<?php //echo $model->enabled == 1 ? Yii::t('phrase', 'Are you sure you want to disabled this item?') : Yii::t('phrase', 'Are you sure you want to enabled this item?')?>
+		<?php //echo $model->status == 1 ? Yii::t('phrase', 'Are you sure you want to unresolved this item?') : Yii::t('phrase', 'Are you sure you want to resolved this item?')?>
+	</div>
+	<div class="dialog-submit">
+		<?php echo CHtml::submitButton($title, array('onclick' => 'setEnableSave()')); ?>
+		<?php echo CHtml::button(Yii::t('phrase', 'Cancel'), array('id'=>'closed')); ?>
+	</div>
+	
+<?php $this->endWidget(); ?>
