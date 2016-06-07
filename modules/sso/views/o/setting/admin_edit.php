b4e5cc8b1eb063c6d03ede477f4b9127def3c998
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-06-07 09:51:58 +0700

    sso: setting source

---
+++ modules/sso/views/o/setting/admin_edit.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Sso Settings (sso-settings)
+ * @var $this SettingController
+ * @var $model SsoSettings
+ * @var $form CActiveForm
+ * version: 0.0.1
+ *
+ * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @copyright Copyright (c) 2016 Ommu Platform (ommu.co)
+ * @created date 27 April 2016, 12:11 WIB
+ * @link http://company.ommu.co
+ * @contect (+62)856-299-4114
+ *
+ */
+
+	$this->breadcrumbs=array(
+		'Sso Settings'=>array('manage'),
+		$model->id=>array('view','id'=>$model->id),
+		'Update',
+	);
+?>
+
+<div class="form" name="post-on">
+	<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+</div>
