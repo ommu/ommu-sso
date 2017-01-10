f6e8a20ca3e50a832c4221af977410a699a1dd64
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-01-10 16:30:46 +0700

    modify url permission

--- modules/inlis/controllers/o/SettingController.php
+++ modules/inlis/controllers/o/SettingController.php
@@ -42,12 +42,10 @@ class SettingController extends Controller
 				$arrThemes = Utility::getCurrentTemplate('admin');
 				Yii::app()->theme = $arrThemes['folder'];
 				$this->layout = $arrThemes['layout'];
-			} else {
-				$this->redirect(Yii::app()->createUrl('site/login'));
-			}
-		} else {
+			} else
+				throw new CHttpException(404, Yii::t('phrase', 'The requested page does not exist.'));
+		} else
 			$this->redirect(Yii::app()->createUrl('site/login'));
-		}
 	}
 
 	/**
