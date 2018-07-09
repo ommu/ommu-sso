86c5a978427ded2f5b5a752563c02bbe5fbe24fc
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-07-09 11:34:27 +0700

    commit

--- views/sync/member/admin_view.php
+++ views/sync/member/admin_view.php
@@ -18,7 +18,7 @@
 	);
 ?>
 
-<?php $this->widget('application.libraries.core.components.system.FDetailView', array(
+<?php $this->widget('zii.widgets.CDetailView', array(
 	'data'=>$model,
 	'attributes'=>array(
 		array(
