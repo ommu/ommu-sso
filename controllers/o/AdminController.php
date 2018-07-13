029ed2215f969655443db6f00abc7e5c25a89dfd
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-07-13 12:57:45 +0700

    move getGridColumn function to trait

--- controllers/o/AdminController.php
+++ controllers/o/AdminController.php
@@ -114,15 +114,7 @@ class AdminController extends Controller
 			$model->attributes=$_GET['SsoUsers'];
 		}
 
-		$columnTemp = array();
-		if(isset($_GET['GridColumn'])) {
-			foreach($_GET['GridColumn'] as $key => $val) {
-				if($_GET['GridColumn'][$key] == 1) {
-					$columnTemp[] = $key;
-				}
-			}
-		}
-		$columns = $model->getGridColumn($columnTemp);
+		$columns = $model->getGridColumn($this->gridColumnTemp());
 
 		$this->pageTitle = Yii::t('phrase', 'Sso Users Manage');
 		$this->pageDescription = '';
--- controllers/sync/MemberController.php
+++ controllers/sync/MemberController.php
@@ -114,15 +114,7 @@ class MemberController extends Controller
 			$model->attributes=$_GET['SyncMembers'];
 		}
 
-		$columnTemp = array();
-		if(isset($_GET['GridColumn'])) {
-			foreach($_GET['GridColumn'] as $key => $val) {
-				if($_GET['GridColumn'][$key] == 1) {
-					$columnTemp[] = $key;
-				}
-			}
-		}
-		$columns = $model->getGridColumn($columnTemp);
+		$columns = $model->getGridColumn($this->gridColumnTemp());
 
 		$this->pageTitle = Yii::t('phrase', 'Inlis Members Manage');
 		$this->pageDescription = '';
