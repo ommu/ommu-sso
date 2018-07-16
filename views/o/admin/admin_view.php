1b696de577c1e610e608c75ac335fc6c53719742
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-07-16 10:42:53 +0700

    modify dateFormat with locale

--- views/o/admin/admin_view.php
+++ views/o/admin/admin_view.php
@@ -39,7 +39,7 @@
 			),
 			array(
 				'name'=>'creation_date',
-				'value'=>!in_array($model->creation_date, array('0000-00-00 00:00:00','1970-01-01 00:00:00','0002-12-02 07:07:12','-0001-11-30 00:00:00')) ? $this->dateFormat($model->creation_date, true) : '-',
+				'value'=>!in_array($model->creation_date, array('0000-00-00 00:00:00','1970-01-01 00:00:00','0002-12-02 07:07:12','-0001-11-30 00:00:00')) ? $this->dateFormat($model->creation_date) : '-',
 			),
 			array(
 				'name'=>'creation_id',
@@ -48,7 +48,7 @@
 			),
 			array(
 				'name'=>'modified_date',
-				'value'=>!in_array($model->modified_date, array('0000-00-00 00:00:00','1970-01-01 00:00:00','0002-12-02 07:07:12','-0001-11-30 00:00:00')) ? $this->dateFormat($model->modified_date, true) : '-',
+				'value'=>!in_array($model->modified_date, array('0000-00-00 00:00:00','1970-01-01 00:00:00','0002-12-02 07:07:12','-0001-11-30 00:00:00')) ? $this->dateFormat($model->modified_date) : '-',
 			),
 			array(
 				'name'=>'modified_id',
